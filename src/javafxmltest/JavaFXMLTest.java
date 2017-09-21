87c90b548cba431d2e989afe5e261e75c09f8db5
Ananya Navelkar <ananya.navelkar@gmail.com>
2017-09-21 15:20:57 +0530

    LoginandSceneChange

--- src/javafxmltest/JavaFXMLTest.java
+++ src/javafxmltest/JavaFXMLTest.java
@@ -10,6 +10,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.Initializable;
+import javafx.fxml.JavaFXBuilderFactory;
+import javafx.scene.layout.AnchorPane;
+//import demo.model.User;
+//import demo.security.Authenticator;
+
 
 /**
  *
@@ -17,22 +26,84 @@ import javafx.stage.Stage;
  */
 public class JavaFXMLTest extends Application {
     
+     private Stage stage;
+//    private User loggedUser;
+    private final double MINIMUM_WINDOW_WIDTH = 390.0;
+    private final double MINIMUM_WINDOW_HEIGHT = 500.0;
+    
     @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+    public void start(Stage primaryStage){
+        /*Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
         
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
-        stage.show();
+        stage.show();*/
+        
+              try {
+            stage = primaryStage;
+            stage.setTitle("EasyPayzee");
+            stage.setMinWidth(MINIMUM_WINDOW_WIDTH);
+            stage.setMinHeight(MINIMUM_WINDOW_HEIGHT);
+            gotoRegister();
+            primaryStage.show();
+        } catch (Exception ex) {
+            Logger.getLogger(JavaFXMLTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        launch(args);
-        
+       
+      Application.launch(JavaFXMLTest.class, (java.lang.String[])null);
     }
+    
+    
+        public void userLogging()
+        {
+            gotoHome();
+         
+    }
+
+        private void gotoHome() {
+        try {
+            HomeController home = (HomeController) replaceSceneContent("Home.fxml");
+            home.setApp(this);
+        } catch (Exception ex) {
+            Logger.getLogger(JavaFXMLTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    
+        private void gotoRegister() {
+        try {
+            RegisterController register = (RegisterController) replaceSceneContent("Register.fxml");
+            register.setApp(this);
+        } catch (Exception ex) {
+            Logger.getLogger(JavaFXMLTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        }
+        
+        
+        private Initializable replaceSceneContent(String fxml) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        InputStream in = JavaFXMLTest.class.getResourceAsStream(fxml);
+        loader.setBuilderFactory(new JavaFXBuilderFactory());
+        loader.setLocation(JavaFXMLTest.class.getResource(fxml));
+        AnchorPane page;
+        try {
+            page = (AnchorPane) loader.load(in);
+        } finally {
+            in.close();
+        } 
+        Scene scene = new Scene(page, 620, 620);
+        stage.setScene(scene);
+        stage.sizeToScene();
+        return (Initializable) loader.getController();
+    }
+
     
 }
--- src/javafxmltest/RegisterController.java
+++ src/javafxmltest/RegisterController.java
@@ -10,6 +10,8 @@ import java.net.URL;
 import java.sql.*;
 import java.sql.Statement;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.animation.FadeTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -17,9 +19,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -29,8 +34,7 @@ import javafx.util.Duration;
  *
  * @author Ananya
  */
-public class RegisterController implements Initializable {
-    
+public class RegisterController extends AnchorPane implements Initializable {
     
     @FXML
         private TextField name;
@@ -40,25 +44,34 @@ public class RegisterController implements Initializable {
         private TextField email;
     @FXML   
         private PasswordField password;
-    @FXML  
-        private TextField balance;
+    @FXML
+        private TextField loginUserName;
+    @FXML
+        private TextField loginPassword;
     
+          private JavaFXMLTest application;
     
-    public void submit() throws SQLException
+    public void registerOnClick() throws SQLException
     {
         DBConnection conn =new DBConnection();   
         Statement stmt=conn.connect().createStatement();
 
         String sql=("insert into user_info"
-                    + "(user_name, user_mobile, user_email, user_password, user_balance)"
-                   + "values('"+name.getText()+"', '"+mobile.getText()+"', '"+email.getText()+"','"+password.getText()+"','"+balance.getText()+"')");
+                    + "(user_name, user_mobile, user_email, user_password)"
+                   + "values('"+name.getText()+"', '"+mobile.getText()+"', '"+email.getText()+"','"+password.getText()+"')");
         
         
         name.setText("");
         mobile.setText("");
         email.setText("");
         password.setText("");
-        balance.setText("");
+        
+        
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("EasyPayzee");
+        alert.setHeaderText("Thanks for registering!");
+        alert.setContentText("You have successfully registered");
+        alert.showAndWait();
         
         
         stmt.executeUpdate(sql);
@@ -66,14 +79,43 @@ public class RegisterController implements Initializable {
          
     }
    
-    public void viewlogin() throws IOException 
+    public void loginOnClick() throws IOException, SQLException, NullPointerException 
     {
+        try {
+            DBConnection conn =new DBConnection();   
+        Statement stmt=conn.connect().createStatement();
+        ResultSet rs = stmt.executeQuery("select * from user_info where user_name='"+loginUserName.getText()+"' and user_password='"+loginPassword.getText()+"'");
+        int count=0;
+           while(rs.next()){
+           count++;
+          }
+        if(count>0){
+            
+            System.out.println("LoggedIn");
+        application.userLogging();  
+        }
+          else{
+           System.out.println("NotRegistered");
+          }
+        //stmt.executeQuery("select * from user_info where user_name='"+loginUserName.getText()+"' and user_password='"+loginPassword.getText()+"'");
+        }
         
+        catch(Exception e){
+            System.out.println(e);
+        }
+        
+    
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         System.out.println("LoadingRegisterView");
-    }    
+    }
+
+       public void setApp(JavaFXMLTest application){
+        this.application = application;
+    }
     
+    
+ 
 }
